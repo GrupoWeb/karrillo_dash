c26258d2096337e7c00faa438a47b5503595b671
Juan Jose Jolon <jjolong@miumg.edu.gt>
2020-07-21 12:15:59 -0600

    fix search and add provider, client

--- resources/js/aplication_k/search_provider.vue
+++ resources/js/aplication_k/search_provider.vue
@@ -1,65 +1,6 @@
 <template>
     <div class="container-md">
-        <!-- <el-card class="box-card">
-            <div slot="header" >
-                <span>Catálogos de Proveedores</span>
-            </div>
-            <el-form ref="form" :model="form" >
-                <el-row :gutter="20">
-                    <el-col :xs="25" :sm="12" :md="12" :lg="12" :xl="12">
-                        <el-form-item label="Nombre Empresa:" >
-                            <el-input v-model="form.company" :ref="'company_name'"  @change="handleInputl('company_name')"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :xs="25" :sm="12" :md="12" :lg="12" :xl="12">
-                        <el-form-item label="Dirección:">
-                            <el-input v-model="form.address"></el-input>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-
-                <el-row :gutter="20">
-                    <el-col :xs="25" :sm="6" :md="8" :lg="6" :xl="8">
-                        <el-form-item label="Nit:">
-                            <el-input v-model="form.nit"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :xs="25" :sm="6" :md="8" :lg="6" :xl="4">
-                        <el-form-item label="Días Credito:">
-                            <el-input v-model="form.credit_days"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :xs="25" :sm="8" :md="8" :lg="8" :xl="4">
-                        <el-form-item label="Teléfono Principal:">
-                            <el-input v-model="form.phone_p"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :xs="25" :sm="8" :md="8" :lg="8" :xl="8">
-                        <el-form-item label="Teléfono Secundario:">
-                            <el-input v-model="form.phone_s"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :xs="25" :sm="8" :md="8" :lg="8" :xl="12">
-                        <el-form-item label="Nombre Contacto:" >
-                            <el-input v-model="form.contact_name" :ref="'contact_name'"  @change="handleInputl('contact_name')"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :xs="25" :sm="8" :md="8" :lg="8" :xl="4">
-                        <el-form-item label="Teléfono Contacto:">
-                            <el-input v-model="form.phone_c"></el-input>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-
-                <el-form-item>
-                    <el-button type="primary" @click="onSubmit" v-loading.fullscreen.lock="fullscreenLoading" element-loading-text="Loading..."
-                               element-loading-spinner="el-icon-loading"
-                               element-loading-background="rgba(0, 0, 0, 0.8)">Guardar</el-button>
-                    <el-button>Cancelar</el-button>
-                </el-form-item>
-            </el-form>
-        </el-card> -->
-        <el-card class="box-card mt-3">
+          <el-card class="box-card mt-3">
             <div slot="header" >
                 <span>Proveedores</span>
             </div>
@@ -218,8 +159,10 @@
                     this.handle_style_table_bootstrap('tables_data');
     
                     let url ='/get_provider_filter';
+                    console.log('data: ', this.search);
                     axios.post(url,{dato: this.search}).then(response => {
                         this.handle_provider = response.data;
+                        console.log(response.data);
                         this.fullscreenLoadingSearch = false;
                     })
                 }
